7cc81374bc138ec70462aad766fd1ffa93172e7d
Stephen J. Maher <s.maher3@lancaster.ac.uk>
2018-11-09 17:47:40 +0000

    stripped whitespace

--- src/select.h
+++ src/select.h
@@ -17,30 +17,30 @@ class Select
 	public:
 		void generate_rand(int _n, int _p, int _N);
 		void generate_normal(int _n, int _p, int _N);
-		
+
 		Solution solve_ip();
 		Solution solve_regret();
-		
+
 		void generate_hard(int _n, int _p, int _N, bool solvecompact = true);
 		void generate_hard_lazy(int _n, int _p, int _N);
 		void generate_hard_alternateheuristic(int _n, int _p, int _N);
 		void generate_hard_LDR(int _n, int _p, int _N);
-		
+
 		void generate_hard_alt(int _n, int _p, int _N);
-		
+
 		void generate_midpoint(int _n, int _p, int _N);
 		void generate_midpointlp(int _n, int _p, int _N);
-		
+
 		void set_budget(int _scenbudget);
-		
+
 		//void generate_hard_heu(int _n, int _p, int _N);
 
 		std::vector<std::vector<double> > get_c();
 		void gen_U();
 		void set_problem(int _n, int _p, int _N, std::vector<std::vector<double> > _c);
-		
+
 		void set_timelimit(double _timelimit);
-		
+
 		void print();
 
 	private:
@@ -48,15 +48,15 @@ class Select
 		int N;
 		std::vector<std::vector<double> > c;
 		std::vector<std::vector<double> > nomc;
-		
+
 		std::vector<std::vector<double> > x;
-		
+
 		//bounds on c
 		std::vector<std::vector<double> > cl;
 		std::vector<std::vector<double> > cu;
 		//constriants of the form <= 1
 		//std::vector<std::vector<double> > constraints;
-		
+
 		double solve_master(bool relax=false);
 		double solve_master_decomp(double lowerbound, bool relax=false);
       double solve_decomp_sub();
